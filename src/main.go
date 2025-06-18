cb1c53059ed77308c80ffaa2aeba520930f11ede
akash23-ai <akashbhardwaj2024@gmail.com>
2025-06-18 22:57:19 +0530

    added listener tcp and grpc server

--- src/main.go
+++ src/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"log"
+	"net"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
-	log.Println("Started Grpc")
+	s := grpc.NewServer()
+
+	// Start listening on port 50051
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	log.Println("gRPC server starting on port 50051...")
+	s.Serve(lis)
+
 }
